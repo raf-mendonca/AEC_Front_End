9673718ac775e65675763009261bcb82ce0da5dc
raf-mendonca <rapedrao@hotmail.com>
2020-01-16 13:54:33 -0500

    Envoie des corrections bootstrap exercices 1 à 7

--- Programmation1/Chapitre5/exercice4/js/script.js
+++ Programmation1/Chapitre5/exercice4/js/script.js
@@ -7,9 +7,10 @@
 
 var tab2Exp = [];
 
-for (var i=0; i<16; i++){
+for (var i=0; i<=16; i++){
     tab2Exp[i] = Math.pow(2,i);
-    }
-document.write(tab2Exp+" </br> ");
+    document.write("2 exposant "+i+ " = "+tab2Exp+"</br>");
+}
+
 
 
