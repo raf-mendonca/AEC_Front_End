114f0030186df296d53aa8e5d5cb1c1944da72ec
raf-mendonca <rapedrao@hotmail.com>
2020-03-29 13:54:51 -0400

    Envoie projet final
    Envoie projet final add @media

--- integration1/Projet Final/js/script.js
+++ integration1/Projet Final/js/script.js
@@ -1,5 +1,9 @@
+/* Projet Final Integration 1
+* Date dernière modification: 2020-03-29
+*Rafael Izaias De Mendonça
 
-/*changement d'images*/
+/*ajustement du temp de changement d'image*/
+
 $('.carousel').carousel({
     interval: 4000
 });
